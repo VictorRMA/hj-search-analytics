8e0e225a1b77ca5b3f0f5b7558f6605026dd280f
Victor Amorim <victor.rma@gmail.com>
2017-09-09 00:17:44 -0300

    Create simple search query store

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,18 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def search
+    if Search.store_query(search_params[:query])
+      puts "Search stored!"
+    else
+      puts "Search not stored!"
+    end
+
+    redirect_to root_path
+  end
+
+  private
+    def search_params
+      params.permit(:query)
+    end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,17 @@
 class Search < ApplicationRecord
+  validates :query, presence: true
+
+  # Create or update Search instance based on query uniqueness
+  # return search instance
+  def self.store_query(query)
+    search = Search.find_by(query: query)
+
+    if search
+      Search.increment_counter(:score, search)
+      search
+    else
+      Search.create(query: query)
+    end
+  end
+
 end
