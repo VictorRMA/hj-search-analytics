eb7340ca87d6475642e540e2801656d431d744a5
Victor Amorim <victor.rma@gmail.com>
2017-09-16 11:49:56 -0300

    Improve javascript to prevent store same querys with trim

--- app/assets/javascripts/articles/index.js
+++ app/assets/javascripts/articles/index.js
@@ -1,14 +1,24 @@
 $("#query-form").on('submit', function(e) {
-  searchArticles($("#query").val());
+  searchArticles(
+    removeSpacesBeforeAndAfter($("#query").val())
+  );
 
   return false;
 });
 
-$("#query").on('keyup', _.debounce(function() {
-  searchArticles($(this).val());
+$("#query").on('keyup', _.debounce(function(e) {
+  if(e.keyCode != 13) {
+    searchArticles(
+      removeSpacesBeforeAndAfter($(this).val())
+    );
+  }
 
   return false;
 }, 3000));
+
+function removeSpacesBeforeAndAfter(query) {
+  return $.trim(query);
+}
 
 function searchArticles(query) {
   $.get({
